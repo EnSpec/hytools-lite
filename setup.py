dc5255de84a33b29a9ba4ccbaaba14c33e6b70f8
Adam Chlus <23018310+adamchlus@users.noreply.github.com>
2022-09-29 10:07:22 -0700

    Removed extra =

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from hytools_lite import __version__
 setup(
     name='hy-tools-lite',
     description= 'HyTools: Hyperspectral image processing library',
-    version== __version__,
-    license='GNUv3',
+    version = __version__,
+    license ='GNU General Public License v3.0',
     url='https://github.com/EnSpec/hytools-lite',
     author = 'Adam Chlus',
     packages=find_packages(),
